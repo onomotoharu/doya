d9cd0f6cfb257cecaaa295801eed57e49b207550
motoharu <onomotoharu@gmail.com>
2013-09-14 20:12:13 +0900

    make rails proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doya::Application.config.secret_key_base = '6650510acbd5ae58233bb643acd035a37d8d7059ec1afe385e55fa00946a4628269754645ef26cad0a0829e620b2a39fcdedffb4e0481b5d21ef30ee3bb04a28'
