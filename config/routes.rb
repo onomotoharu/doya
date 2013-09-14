9ea4c75a7e5268d578fda630d4d6daa9894de2aa
motoharu <onomotoharu@gmail.com>
2013-09-15 00:34:01 +0900

    session implemetend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Doya::Application.routes.draw do
+  devise_for :users
   resources :photos
-
-  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
